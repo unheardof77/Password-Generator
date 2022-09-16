cdf96f4fa01020d8830f917ab3e2f6f8a70fc3aa
Morgan Tolman <morgan.tolman04@gmail.com>
2022-09-16 10:01:49 -0700

    Added random function.  Known bug, password does not include all characters. Safe revert point.

--- script.js
+++ script.js
@@ -1,29 +1,38 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-
+let randomPassword = "";
+let randomArray = [ ];
 function generatePassword(){
-    questionReturn1 = prompt("What would you like the length of your password to be?");
+    let = questionReturn1 = prompt("What would you like the length of your password to be?");
     if ( questionReturn1 <=128 && questionReturn1 >= 8){
-        randomArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
-
+      
     } else {
       alert("Please select a value between 8 and 128.");
     };
     
-    questionReturn2 = confirm("Would you like special characters in your password?");
+    numeric = confirm("Would you like numeric characters in your password?");
+    if (numeric){
+      randomArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+
+    };
+
+    
+
+    
+    let = questionReturn2 = confirm("Would you like special characters in your password?");
     if (questionReturn2){
       questionReturn2 = [ "~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "?" ];
       randomArray.push(... questionReturn2)
     };
     
 
-    questionReturn3 = confirm("Would you like uppercase letters included in your password?");
+    let = questionReturn3 = confirm("Would you like uppercase letters included in your password?");
     if (questionReturn3){
       questionReturn3 = ["A", "B", "C", "D", "E", "F", "G"]
       randomArray.push(... questionReturn3)
     };
     
-    questionReturn4 = confirm("Would you like lowercase letters included in your password?");
+    let = questionReturn4 = confirm("Would you like lowercase letters included in your password?");
     if (questionReturn4){
       questionReturn4 = ["a", "b", "c", "d", "e", "f", "g"]
       randomArray.push(... questionReturn4)
@@ -31,9 +40,9 @@ function generatePassword(){
     console.log(randomArray.length);
     for ( i = 0; i < questionReturn1; i++){
 
-      randomPassword = randomArray[Math.floor(Math.random() * questionReturn1)];
+       randomPassword += randomArray[Math.floor(Math.random() * questionReturn1)+ 0];
     };
-
+    
   };
 
 // Write password to the #password input
