f633ba882869d29cb2d81ac1481137caa30af305
Morgan Tolman <morgan.tolman04@gmail.com>
2022-09-15 18:21:26 -0700

    Setup getting user input.  Safe revert point.

--- script.js
+++ script.js
@@ -1,14 +1,44 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword(){
+    questionReturn1 = prompt("What would you like the length of your password to be?");
+    if ( questionReturn1 <=128 && questionReturn1 >= 8){
+        randomArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+
+    } else {
+      alert("Please select a value between 8 and 128.");
+    };
+    
+    questionReturn2 = confirm("Would you like special characters in your password?");
+    if (questionReturn2){
+      questionReturn2 = [ "~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "?" ];
+      randomArray.push(... questionReturn2)
+    };
+    
+
+    questionReturn3 = confirm("Would you like uppercase letters included in your password?");
+    if (questionReturn3){
+      questionReturn3 = ["A", "B", "C", "D", "E", "F", "G"]
+      randomArray.push(... questionReturn3)
+    };
+    
+    questionReturn4 = confirm("Would you like lowercase letters included in your password?");
+    if (questionReturn4){
+      questionReturn4 = ["a", "b", "c", "d", "e", "f", "g"]
+      randomArray.push(... questionReturn4)
+    };
+    console.log(randomArray)
+  };
+
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  generatePassword();
+  var password = placeHolder;
   var passwordText = document.querySelector("#password");
-
+  
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", generatePassword);
